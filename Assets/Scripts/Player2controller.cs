95f443c2fd7453ad90b61f84450cbfff7f74e0bb
Ian Smith <alexander.leavitt@meritknights.com>
2022-11-18 13:28:07 -0700

    Player movement finished

--- Assets/Scripts/Player2controller.cs
+++ Assets/Scripts/Player2controller.cs
@@ -8,7 +8,7 @@ public class Player2controller : MonoBehaviour
     private bool isGrounded;
     private Rigidbody2D rb;
     //Public variables set in Unity Inspector
-    public float xSpeed;
+    public float xSpeed2;
     public float jumpStrength;
     //initialize private variables in start
     // Start is called before the first frame update
@@ -20,9 +20,9 @@ public class Player2controller : MonoBehaviour
 
     // Update is called once per frame, Fixed used for physics
     void FixedUpdate()
-    {
-        float xHat = new Vector2( Input.GetAxis("Horizontal2") , 0).normalized.x;
-        float vx = xHat * xSpeed;
+    {   
+        float xHat = new Vector2( Input.GetAxis("Player2Hor") , 0).normalized.x;
+        float vx = xHat * xSpeed2;
         if (isGrounded) {
             rb.AddForce(transform.right * vx);
         } else {
@@ -37,7 +37,6 @@ public class Player2controller : MonoBehaviour
             rb.AddForce(transform.up * vy);
         }
     }
-
     void OnCollisionEnter2D(Collision2D collision) {
         //Get rid of this when no longer necessary
         Debug.Log(collision.gameObject.tag);
