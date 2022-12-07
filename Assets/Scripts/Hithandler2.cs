afcd398de2dd03416877842579b88b2dbf9e2532
RefractedPurpose <alexander.leavitt@meritknights.com>
2022-12-07 13:52:42 -0700

    Finished attacks, working on options for separate ones

--- Assets/Scripts/Hithandler2.cs
+++ Assets/Scripts/Hithandler2.cs
@@ -6,7 +6,7 @@ public class Hithandler2 : MonoBehaviour
     public int damage;
     public float atk1x;
     public float atk1y;
-    public float hitTime;
+    public float hitTime1;
     private float hitTimeCounter;
     public float launchVert;
     public float launchHoriz;
@@ -14,6 +14,13 @@ public class Hithandler2 : MonoBehaviour
     private Vector2 boxPos = new Vector2 (0, 0);
     private Transform parentTransform;
     private bool fireAllow;
+    public int damage2;
+    public float atk2x;
+    public float atk2y;
+    public float hitTime2;
+    public float launchHoriz2;
+    public float launchVert2;
+    public Vector2 atk2Pos;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,23 +32,40 @@ public class Hithandler2 : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if(Input.GetAxis ("Fire") != 0 && fireAllow) {
+        if(Input.GetAxis ("Fire") >= 0.1 && fireAllow) {
             fireAllow = false;
             transform.localScale = new Vector3(atk1x, atk1y);
-            transform.position = new Vector3(atk1Pos.x + parentTransform.position.x, atk1Pos.y + parentTransform.position.y, 0);
+            transform.position = new Vector3 (atk1Pos.x + parentTransform.position.x, atk1Pos.y + parentTransform.position.y, 0);
+            // transform.position = new Vector3 (parentTransform.position.x, parentTransform.position.y, 0);
         }
-        if(hitTimeCounter <= hitTime){
+        
+        if(hitTimeCounter <= hitTime1 && !fireAllow){
             hitTimeCounter++;
-        }
-        if(hitTimeCounter == hitTime){
+            Debug.Log("hit time count");
+        } else if (!fireAllow) {
             transform.localScale = new Vector3(0.1f, 0.1f);
             hitTimeCounter = 0; 
-            transform.position = parentTransform.position;
+            transform.position = new Vector3 (parentTransform.position.x - atk1Pos.x, parentTransform.position.y - atk1Pos.y, 0);
             fireAllow = true;
+            Debug.Log ("hit timer reached");
         }
-        if(hitTimeCounter >= hitTime){
-            hitTimeCounter = 0;
-        }
+        // if(Input.GetAxis ("Fire") <= 0 && fireAllow) {
+        //     fireAllow = false;
+        //     transform.localScale = new Vector3(atk2x, atk2y);
+        //     transform.position = new Vector3 (atk2Pos.x + parentTransform.position.x, atk2Pos.y + parentTransform.position.y, 0);
+        //     // transform.position = new Vector3 (parentTransform.position.x, parentTransform.position.y, 0);
+        // }
+        
+        // if(hitTimeCounter <= hitTime2 && !fireAllow){
+        //     hitTimeCounter++;
+        //     Debug.Log("hit time count");
+        // } else if (!fireAllow) {
+        //     transform.localScale = new Vector3(0.1f, 0.1f);
+        //     hitTimeCounter = 0; 
+        //     transform.position = new Vector3 (parentTransform.position.x - atk2Pos.x, parentTransform.position.y - atk2Pos.y, 0);
+        //     fireAllow = true;
+        //     Debug.Log ("hit timer reached");
+        // }
                 parentTransform = gameObject.GetComponentInParent(typeof(Transform)) as Transform;
                 // Debug.Log(parentTransform.position);
     }
